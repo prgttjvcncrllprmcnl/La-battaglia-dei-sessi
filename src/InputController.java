cb3b1b3c226d2a79fc4a8347aa5869142caccde6
Daniel <hrituc@hotmail.it>
2017-06-15 03:14:55 +0200

    Update InputController.java

--- src/InputController.java
+++ src/InputController.java
@@ -2,23 +2,33 @@
  * Created by Nikita on 11/06/2017.
  */
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import org.jfree.ui.RefineryUtilities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static java.lang.System.out;
 
-public class InputController extends Test{
+
+/**
+ * Classe che gestisce la finestra di input dei valori e i due grafici
+ */
+public class InputController {
+    //valori di default
     private int def_a = new Integer(10000);
     private int def_b = new Integer(10000);
     private int def_c = new Integer(10000);
     private int def_d = new Integer(10000);
-    private int def_a1 = new Integer(30);
-    private int def_b1 = new Integer(40);
-    private int def_c1 = new Integer(6);
+    private int def_a1 = new Integer(15);
+    private int def_b1 = new Integer(20);
+    private int def_c1 = new Integer(3);
     boolean bool = true;
     static Population p;
+    private List<int[]> popArray = new ArrayList<>();
 
     @FXML
     private TextField tf_morigerati;
@@ -51,15 +61,26 @@ public class InputController extends Test{
         String a1 = tf_premiogenerazionefigli.getText();
         String b1 = tf_costocrescita.getText();
         String c1 = tf_costocorteggiamento.getText();
-        //Trasformo le stringhe in int
-        int aa = Integer.parseInt(a);
-        int bb = Integer.parseInt(b);
-        int cc = Integer.parseInt(c);
-        int dd = Integer.parseInt(d);
-        int aa1 = Integer.parseInt(a1);
-        int bb1 = Integer.parseInt(b1);
-        int cc1 = Integer.parseInt(c1);
-        //Metto a condizione che se non si rispettano i valori di immissione il programma partirà con valori di default
+        int aa, bb, cc, dd, aa1, bb1, cc1;
+        //Trasformo le stringhe in int se non sono stringhe vuote
+        if (!(a.equals("") || b.equals("") || c.equals("") || d.equals("") || a1.equals("") || b1.equals("") || c1.equals(""))) {
+            aa = Integer.parseInt(a);
+            bb = Integer.parseInt(b);
+            cc = Integer.parseInt(c);
+            dd = Integer.parseInt(d);
+            aa1 = Integer.parseInt(a1);
+            bb1 = Integer.parseInt(b1);
+            cc1 = Integer.parseInt(c1);
+        } else { //altrimenti mando in input i valori di default
+            aa = def_a;
+            bb = def_b;
+            cc = def_c;
+            dd = def_d;
+            aa1 = def_a1;
+            bb1 = def_b1;
+            cc1 = def_c1;
+        }
+        //se si inseriscono valori negativi per le popolazioni, il programma partirà con valori di default
         if (aa < 0 || bb < 0 || cc < 0 || dd < 0) {
             aa = def_a;
             bb = def_b;
@@ -72,51 +93,99 @@ public class InputController extends Test{
         p = new Population(aa,bb,dd,cc);
         p.setValues(aa1,bb1,cc1);
 
-        //chiudo la finestra di input
-        finestra.close();
-
-        Graphics.PieChartDemo2 pie = new Graphics.PieChartDemo2("Popultion");
+        //grafico a torta
+        Graphics.PieChartDemo2 pie = new Graphics.PieChartDemo2("Population");
         pie.pack();
         RefineryUtilities.positionFrameOnScreen(pie,0.1,0.4);
         pie.setVisible(true);
 
-        final Graphics.DynamicLineAndTimeSeriesChart demo=new Graphics.DynamicLineAndTimeSeriesChart("Popultion");
+        //grafico a linee
+        final Graphics.DynamicLineAndTimeSeriesChart demo=new Graphics.DynamicLineAndTimeSeriesChart("Population");
         demo.pack();
         RefineryUtilities.positionFrameOnScreen(demo,0.9,0.4);
         demo.setVisible(true);
-        int n = 0;
-        while(bool) {
-            if (n == 0)
-                p.evolve();
-            printPop(p, p.currentPopulation());
 
+        /*per una migliore visibilità dell'evoluzione nel grafico, la funzione che fa evolvere la popolazione è chiamata
+        ogni 200 iterazioni */
+        int n = 1;
+        boolean stabilized = false;
+        boolean clear = false;
+        while(bool) {
+            if (n == 0) {
+                p.evolve();
+                if (!stabilized) {
+                    if (popArray.size() < 20) {
+                        popArray.add(p.currentPopulation());
+                    } else if (popArray.size() >= 20) {
+                        popArray.remove(0);
+                        popArray.add(p.currentPopulation());
+                        boolean stable = p.checkForStability(popArray);
+                        if (stable) {
+                            Random r = new Random();
+                            int i = r.nextInt(3);
+                            switch (i) {
+                                case 0: {
+                                    p.incrementPopulation(50000, "M");
+                                    out.println("La popolazione era stabile. Ho aggiunto 50000 Morigerati, vediamo come va...");
+                                    stabilized = true;
+                                    clear = true;
+                                    break;
+                                }
+                                case 1: {
+                                    p.incrementPopulation(50000, "P");
+                                    out.println("La popolazione era stabile. Ho aggiunto 50000 Prudenti, vediamo come va...");
+                                    stabilized = true;
+                                    clear = true;
+                                    break;
+                                }
+                                case 2: {
+                                    p.incrementPopulation(50000, "A");
+                                    out.println("La popolazione era stabile. Ho aggiunto 50000 Avventurieri, vediamo come va...");
+                                    stabilized = true;
+                                    clear = true;
+                                    break;
+                                }
+                                case 3: {
+                                    p.incrementPopulation(50000, "S");
+                                    out.println("La popolazione era stabile. Ho aggiunto 50000 Spregiudicate, vediamo come va...");
+                                    stabilized = true;
+                                    clear = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (stabilized) {
+                    if (clear) {
+                        popArray.clear();
+                        clear = false;
+                    }
+                    if (popArray.size() < 20) {
+                        popArray.add(p.currentPopulation());
+                    } else if (popArray.size() >= 20) {
+                        popArray.remove(0);
+                        popArray.add(p.currentPopulation());
+                        boolean stable = p.checkForStability(popArray);
+                        if (stable) {
+                            out.println("Di nuovo stabili! :)");
+                            stabilized = false;
+                        }
+                    }
+                }
+            }
             Graphics.PieChartDemo2 demo2=new Graphics.PieChartDemo2("Popolazione");
             pie.setContentPane(demo2.getContentPane());
             pie.setVisible(true);
-            /*
-            if (p.currentPopulation()[0] == 0 || p.currentPopulation()[1] == 0 || p.currentPopulation()[2] == 0 || p.currentPopulation()[3] == 0) {
-                break;
-            }
-            */
             n = (n+1)%200;
         }
     }
 
-    private static void printPop(Population p, int[] n) {
-        double morp = p.getPercentage(Individual.Type.M);
-        double avvp = p.getPercentage(Individual.Type.A);
-        double prup = p.getPercentage(Individual.Type.P);
-        double sprp = p.getPercentage(Individual.Type.S);
-        String s = "[M: "+n[0]+"; A: "+n[1]+"; P: "+n[2]+"; S: "+n[3]+"]";
-        String per = "[M: "+morp+"; A: "+avvp+"; P: "+prup+"; S: "+sprp+"]";
-        String perPrudDonne = "  ---  %P: ";
-        String perMorUomini = "; %M: ";
-        out.print(s+ "  ---  ");
-        out.print("Percentuali: "+per);
-        double[] d = faiIlPrint(n);
-        out.println(perPrudDonne+d[0]+perMorUomini+d[1]);
-    }
-
+    /**
+     * Metodo chiamato dai grafici per ottenere le percentuali relative dei tipi
+     * @param n l'array d interi che rappresenta il numero di individui per ciascun tipo
+     * @return un array di double contenente le percentuali relative dei tipi
+     */
     public static double[] faiIlPrint(int[] n) {
         double[] d = new double[4];
         d[0] = ((double)n[2])/((double)n[2]+(double)n[3]); //prudenti
@@ -125,4 +194,5 @@ public class InputController extends Test{
         d[3] = ((double)n[3])/((double)n[3]+(double)n[2]); //spregiudicate
         return d;
     }
+
 }
